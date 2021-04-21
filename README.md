20d1aa137ade715a7730ead2d2526f2e3f8d82fd
cozyviking <george.cagle@gmail.com>
2021-04-21 08:15:32 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # golang-helloworld
+
+This is a simple "Hello World" example using golang. 
+
+## Running
+
+go run hello.go
---
+++ src/hello_world/main.go
@@ -0,0 +1,9 @@
+package main
+
+// fmt module needed for Println
+import "fmt"
+
+// main is the primary function
+func main() {
+	        fmt.Println("Hello, World")
+}
